4385e47d5bc57ae158af8a59890491f33d34f838
Peter van Dooren <p.v.dooren@tue.nl>
2024-03-08 10:17:19 +0100

    update example for debugging

--- examples/test_io.cpp
+++ examples/test_io.cpp
@@ -9,7 +9,7 @@ int main()
     emc::IO io;
 
     // Create Rate object, which will help using keeping the loop at a fixed frequency
-    emc::Rate r(100);
+    emc::Rate r(10);
 
     // Loop while we are properly connected
     while(io.ok())
@@ -20,10 +20,14 @@ int main()
         emc::OdometryData odom;
         if (io.readOdometryData(odom))
             std::cout << "Odometry: " << odom.x << ", " << odom.y << ", " << odom.a << std::endl;
+        else
+            std::cout << "No Odom received" << std::endl;
 
         emc::LaserData scan;
         if (io.readLaserData(scan))
             std::cout << "Laser: " << scan.ranges.size() << " beams" << std::endl;
+        else
+            std::cout << "No laser received" << std::endl;
 
         // Sleep remaining time
         r.sleep();
