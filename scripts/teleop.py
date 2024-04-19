803fe15e359ae86bfda27b1394ee6e064a9dc51c
Peter van Dooren <p.v.dooren@tue.nl>
2024-04-19 16:34:51 +0200

    change teleop keybinding

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -14,42 +14,28 @@ msg = """
 Reading from the keyboard  and Publishing to Twist!
 ---------------------------
 Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+   q    w    e
+   a    s    d
 
-9   : rotate counterclockwise
-0   : rotate clockwise
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
+r/f : increase/decrease max speeds by 10%
 anything else : stop
 
 CTRL-C to quit
 """
 
 moveBindings = {
-        'm':(-0.7,0.7,0),
-        ',':(-1,0,0),
-        '.':(-0.7,-0.7,0),
-        'j':(0,1,0),
-        'l':(0,-1,0),
-        'u':(0.7,0.7,0),
-        'i':(1,0,0),
-        'o':(0.7,-0.7,0),
-        '9':(0,0,1),
-        '0':(0,0,-1)
-           }
+        'w':(1,0,0),
+        's':(-1,0,0),
+        'a':(0,0,1),
+        'd':(0,0,-1),
+        'q':(1, 0, 1),
+        'e':(1, 0, -1)
+        }
 
 speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
-          }
+        'r':(1.1,1.1),
+        'f':(.9,.9),
+         }
 
 
 class PublishThread(threading.Thread):
